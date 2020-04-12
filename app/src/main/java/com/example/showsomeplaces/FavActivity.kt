b9d9d72a4d8a574ee88738695ceca27586adcac2
pauwelcz <bejbojsu8m>
2020-04-12 21:45:24 +0200

    Created Fav And Add activities

---
+++ app/src/main/java/com/example/showsomeplaces/FavActivity.kt
@@ -0,0 +1,12 @@
+package com.example.showsomeplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fav)
+    }
+}
