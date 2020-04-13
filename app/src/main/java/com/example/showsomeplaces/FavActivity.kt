f4e318e6177e93643e73f1b237f7261c03027405
pauwelcz <bejbojsu8m>
2020-04-13 17:31:45 +0200

    Starting new activity on button click

--- app/src/main/java/com/example/showsomeplaces/FavActivity.kt
+++ app/src/main/java/com/example/showsomeplaces/FavActivity.kt
@@ -1,12 +1,23 @@
 package com.example.showsomeplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_fav.*
 
 class FavActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fav)
+
+        home_button.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Favourite activity", null).show()
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/showsomeplaces/MainActivity.kt
+++ app/src/main/java/com/example/showsomeplaces/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.showsomeplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fav_button.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Favourite activity", null).show()
+
+            val intent = Intent(this, FavActivity::class.java)
+            startActivity(intent)
+        }
+
+        add_button.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Favourite activity", null).show()
+
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
