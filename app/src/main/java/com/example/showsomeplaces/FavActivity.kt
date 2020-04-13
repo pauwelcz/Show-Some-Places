f08d61edf13eb5e2cfd0a8ea9751a21997bdd15f
pauwelcz <bejbojsu8m>
2020-04-13 22:57:27 +0200

    Added Recycler View, version 1

--- app/src/main/java/com/example/showsomeplaces/FavActivity.kt
+++ app/src/main/java/com/example/showsomeplaces/FavActivity.kt
@@ -3,14 +3,33 @@ package com.example.showsomeplaces
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.showsomeplaces.PlacesAdapter
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_fav.*
 
 class FavActivity : AppCompatActivity() {
 
+    val places: ArrayList<String> = ArrayList()
+
+    val animals: ArrayList<String> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fav)
+
+        // Loads animals into the ArrayList
+        addAnimals()
+
+        // Creates a vertical Layout Manager
+        fav_places_list.layoutManager = LinearLayoutManager(this)
+
+        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
+//        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
+
+        // Access the RecyclerView Adapter and load the data into it
+        fav_places_list.adapter = PlacesAdapter(animals, this)
+
 
         home_button.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -19,5 +38,40 @@ class FavActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun addAnimals() {
+        animals.add("dog")
+        animals.add("cat")
+        animals.add("owl")
+        animals.add("cheetah")
+        animals.add("raccoon")
+        animals.add("bird")
+        animals.add("snake")
+        animals.add("lizard")
+        animals.add("hamster")
+        animals.add("bear")
+        animals.add("lion")
+        animals.add("tiger")
+        animals.add("horse")
+        animals.add("frog")
+        animals.add("fish")
+        animals.add("shark")
+        animals.add("turtle")
+        animals.add("elephant")
+        animals.add("cow")
+        animals.add("beaver")
+        animals.add("bison")
+        animals.add("porcupine")
+        animals.add("rat")
+        animals.add("mouse")
+        animals.add("goose")
+        animals.add("deer")
+        animals.add("fox")
+        animals.add("moose")
+        animals.add("buffalo")
+        animals.add("monkey")
+        animals.add("penguin")
+        animals.add("parrot")
     }
 }
