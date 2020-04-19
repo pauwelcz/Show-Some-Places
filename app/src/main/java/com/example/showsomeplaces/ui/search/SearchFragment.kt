c594d0e1ad45cea745c52a1f25ce1cc9b108a547
pauwelcz <bejbojsu8m>
2020-04-19 21:11:55 +0200

    Created icon for editing and deleting

--- app/src/main/java/com/example/showsomeplaces/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/showsomeplaces/ui/search/SearchFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProviders
 import com.example.showsomeplaces.R
+import kotlinx.android.synthetic.main.fragment_detail.view.*
+import kotlinx.android.synthetic.main.fragment_search.view.*
 
 class SearchFragment : Fragment() {
-
-
     private lateinit var searchViewModel: SearchViewModel
 
     override fun onCreateView(
@@ -20,13 +23,27 @@ class SearchFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        retainInstance = true
         searchViewModel =
             ViewModelProviders.of(this).get(SearchViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_search, container, false)
-        val textView: TextView = root.findViewById(R.id.text_search)
-        searchViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val categoryArray = arrayOf("Doma", "Práce", "Osobní")
+        context?.let { context ->
+            root.poi_spinner.adapter =
+                ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, categoryArray)
+
+            root.poi_spinner.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onNothingSelected(parent: AdapterView<*>?) {}
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {}
+                }
+        }
         return root
     }
 }
